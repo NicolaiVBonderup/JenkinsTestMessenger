aee814a511bf0ac47868ab98266fec041e4baf2e
NicolaiVBonderup <nvbonderup@gmail.com>
2018-01-23 13:28:39 +0100

    Added dummy test

--- src/test/java/dk/profac/jenkinstestmessenger/JenkinsTestMessengerApplicationTests.java
+++ src/test/java/dk/profac/jenkinstestmessenger/JenkinsTestMessengerApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +13,7 @@ public class JenkinsTestMessengerApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertThat(true, is(true));
 	}
 
 }
